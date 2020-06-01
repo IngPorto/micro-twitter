789192a79505a8a9680104596619ee75f6a64a96
david <user@DESKTOP-0T2RNIV>
2020-05-31 23:03:48 -0500

    configuration for deploy to Heroku

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect ('mongodb://localhost/micro-twitter', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+// 'mongodb://localhost/micro-twitter'
+mongoose.connect ('mongodb+srv://application:<password>@cluster0-hllop.mongodb.net/micro-twitter?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then( db => console.log('BASE DE DATOS: Conectada'))
     .catch( err => console.error(err))
 
