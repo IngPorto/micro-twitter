9543b192d648e02428cc0824acadec1146795543
david <user@DESKTOP-0T2RNIV>
2020-06-02 23:38:06 -0500

    allowing tweets with image or without image

--- src/api-routes/twit.routes.js
+++ src/api-routes/twit.routes.js
@@ -31,13 +31,13 @@ let corsOptions = {
 }
 // Enabling CORS Pre-Flight. Necesario cuando el cliente envía datos y el servidor mantiene la sesión por cookies
 //router.options('/:skip/:limit', cors(corsOptions))   // Actualización de datos de tarea y Eliminación
+router.options('/noImageTwit', cors(corsOptions))      // Creación de twit sin imagen, solo texto
 router.options('/like/:id', cors(corsOptions))      // Asignar un like
 router.options('/share/:id', cors(corsOptions))     // Asignar un share
 router.options('/comment/:id', cors(corsOptions))   // Agregar un comentario
 router.options('/comments', cors(corsOptions))   // Agregar un comentario
 router.options('/user/:id', cors(corsOptions))   // Actualización de datos de tarea y Eliminación
 router.options('/:id', cors(corsOptions))   // Actualización de datos de tarea y Eliminación
-router.options('/noImageTwit', cors(corsOptions))      // Creación de twit sin imagen, solo texto
 router.options('/test', cors(corsOptions))      // Creación de tarea
 router.options('/', cors(corsOptions))      // Creación de tarea
 // ---END-Configuración-CORS---
